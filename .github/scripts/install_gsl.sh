b2ee693815fdd135746c31577e92172b5b31ae02
Håkon Hægland <hakon.hagland@gmail.com>
2021-01-14 01:03:56 +0100

    Auto commit: Thu Jan 14 01:03:56 2021

--- .github/scripts/install_gsl.sh
+++ .github/scripts/install_gsl.sh
@@ -1,15 +1,12 @@
 #! /bin/bash
 
-GSL_VERSION="2.6"
 GSL_MIRROR=http://mirrors.kernel.org/gnu/gsl
-GSL_INST_DIR="$GITHUB_WORKSPACE"/"gsl"
-GSL_SRC_DIR="$GITHUB_WORKSPACE"/"gsl-source"
 TARBALL_GSL=gsl-"$GSL_VERSION"
 TARBALL="$TARBALL_GSL".tar.gz
 
 mkdir -p "$GSL_SRC_DIR"
 cd "$GSL_SRC_DIR"
-wget "$GSL_MIRROR"/"$TARBALL" --retry-connrefused --timeout=900 
+wget "$GSL_MIRROR"/"$TARBALL" --retry-connrefused --timeout=900
 tar zxvf "$TARBALL"
 cd "$TARBALL_GSL"
 ./configure --prefix "$GSL_INST_DIR"/"$TARBALL_GSL"
