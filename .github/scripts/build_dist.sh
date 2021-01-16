a705ccc7ad92fd0a95f880de9d31ea9e054386c9
Håkon Hægland <hakon.hagland@gmail.com>
2021-01-16 08:53:09 +0100

    Auto commit: Sat Jan 16 08:53:09 2021

---
+++ .github/scripts/build_dist.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+export LD_LIBRARY_PATH="$GSL_INST_DIR"/"$TARBALL_GSL"/lib
+export PATH="$GSL_INST_DIR"/"$TARBALL_GSL"/bin:"$PATH"
+cpan App::cpanminus
+cpanm -n Net::SSLeay
+cpanm Alien::GSL
+cpanm Module::Build
+mkdir xs
+perl Build.PL
+./Build installdeps --cpan_client cpanm
+./Build
+./Build test
+./Build dist
+DISTNAME=$(ls -1 Math*.tar.gz)
+echo "MATH_GSL_DISTNAME=$DISTNAME" >> $GITHUB_ENV
+echo "$DISTNAME" > math-gsl-dist-name.txt
