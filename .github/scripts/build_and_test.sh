488305e8c40fb4f8f6622083527eebcc86074f55
Håkon Hægland <hakon.hagland@gmail.com>
2021-01-30 00:22:50 +0100

    Auto commit: Sat Jan 30 00:22:50 2021

--- .github/scripts/build_and_test.sh
+++ .github/scripts/build_and_test.sh
@@ -2,9 +2,24 @@
 
 export LD_LIBRARY_PATH=${GSL_INST_DIR}/${TARBALL_GSL}/lib
 export PATH="$GSL_INST_DIR"/"$TARBALL_GSL"/bin:"$PATH"
-cpanm -n Net::SSLeay
+cpanm -vn Net::SSLeay
 cpanm -n Alien::Build
-cpanm Alien::GSL
+if [[ $GITHUB_WORKFLOW == *"windows"* ]] ; then
+    tempdir=$(mktemp -d)
+    curdir=$PWD
+    cd $tempdir
+    git clone https://github.com/PerlAlien/Alien-GSL.git
+    cd Alien-GSL
+    git fetch origin pull/8/head:pr7_mm
+    git checkout pr7_mm
+    perl Makefile.PL
+    make
+    make test
+    make install
+    cd $curdir
+else
+    cpanm Alien::GSL
+fi
 cpanm Module::Build
 mkdir -p xs
 perl Build.PL
